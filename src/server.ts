76455cff42b8e76adbba0764a3675622675facb0
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-10-14 11:36:45 +0200

    init New

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import App from '@/app';
-import AuthRoute from '@routes/auth.route';
-import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
-import validateEnv from '@utils/validateEnv';
+import app from "./app";
 
-validateEnv();
-
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
-
-app.listen();
+app.listen(3333);
