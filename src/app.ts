2fc5bb20c5a1f2233ca00d147e7777fcad95cc34
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-10-17 18:15:17 +0200

    update two

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,35 @@
 import express from "express";
+import cors from 'cors';
 
 import routes from "./routes";
+import { Routes } from "./interfaces/routes.interface";
 
 class App {
   public server;
+  public app: express.Application;
 
-  constructor() {
+
+  constructor(routes: Routes[]) {
     this.server = express();
 
     this.middlewares();
-    this.routes();
+    this.initializeRoutes(routes);
   }
 
   middlewares() {
+    this.app.use(cors());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
   }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach(route => {
+      this.app.use('/', route.router);
+    });
+  }
 }
 
-export default new App().server;
+export default App;
