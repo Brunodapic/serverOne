11967a21081fbcaf9d73554c933c2b1ae8603413
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-10-20 13:32:28 +0200

    bd connected

--- src/routes.ts
+++ src/routes.ts
@@ -8,11 +8,13 @@ const routes = Router();
 
 const pool = new Pool({
   user: "web2db_yg2d_user",
-  host: "dpg-cd4l29qrrk02t5fabg8g-a",
+  host: "dpg-cd4l29qrrk02t5fabg8g-a.frankfurt-postgres.render.com",
   database: "web2db_yg2d",
   password: "OAwjGQgmQBdnik9qejHiP7EMttUueP3H",
   port: 5432,
+  ssl: true
 });
+
 
 
 routes.get("/", authMiddleware, (req, res) => {
@@ -21,7 +23,7 @@ routes.get("/", authMiddleware, (req, res) => {
 
 routes.get("/db", async (req, res) => {
   console.log("yes db")
-  const results = await pool.query("SELECT * from people");
+  const results = await pool.query("SELECT * FROM public.user_table ORDER BY user_id ASC ");
   console.log( results)
   return res.json({ data: results });
 
