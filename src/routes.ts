f54c0dfa08e5d6c452171055b921f59d8255bce5
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-10-23 17:57:44 +0200

    comment api

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authMiddleware from "./middlewares/auth.middleware";
 import { Pool } from "pg";
 import dotenv from "dotenv";
+import randomstring from "randomstring";
 
 dotenv.config();
 const routes = Router();
@@ -45,6 +46,46 @@ routes.get("/db/game/:id", async (req, res) => {
   const dataSql = results.rows
   console.log( results.rows)
   return res.json({ data: results.rows });
+
+});
+
+//KOMENTARI
+
+routes.get("/db/comment/:week", async (req, res) => {
+  console.log("yes db")
+
+  const results = await pool.query("SELECT * FROM public.comments where comments.week='"+req.params.week+"' ");
+  console.log( results.rows)
+  return res.json({ data: results.rows });
+
+});
+
+routes.post("/db/comment", async (req, res) => {
+  console.log("yes db")
+  const id = randomstring.generate(20);
+  const results = await pool.query("INSERT INTO comments VALUES ('"+id+"', '"+req.body.email+"' , '"+req.body.comment+"', '"+req.body.week+"' )");
+  console.log( results)
+  return res.json({ data: results });
+
+});
+
+routes.put("/db/comment/:id", async (req, res) => {
+  console.log("yes db")
+  const sql="UPDATE comments SET comment = '"+req.body.comment+"' WHERE comment_id = '"+req.body.comment_id+"'"
+  console.log(sql)
+  const results = await pool.query(sql);
+  console.log( results)
+  return res.json({ data: results });
+
+});
+
+routes.delete("/db/comment/:id", async (req, res) => {
+  console.log("yes db")
+  const sql = "DELETE FROM comments WHERE comment_id = '"+req.body.comment_id+"'"
+  console.log(sql)
+  const results = await pool.query(sql);
+  console.log( results)
+  return res.json({ data: results });
 
 });
 
